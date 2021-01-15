591f723f73eb1e3c05a7defcbad961fc36104c9e
Rob Hess <robwhess@gmail.com>
2021-01-14 16:29:43 -0800

    Add code from 1/14 lecture.

--- app/src/main/java/com/example/android/cs492todos/MainActivity.java
+++ app/src/main/java/com/example/android/cs492todos/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.android.cs492todos;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText todoEntryET;
+    private TextView todoListTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.todoListTV = findViewById(R.id.tv_todo_list);
+        this.todoListTV.setText("TODO: finish my TODO app");
+
+        this.todoEntryET = findViewById(R.id.et_todo_entry_box);
+
+        Button addTodoBtn = findViewById(R.id.btn_add_todo);
     }
 }
