cb63de282326f75c3045bb3c306827a5fa71d6b7
Rob Hess <robwhess@gmail.com>
2021-01-19 14:35:20 -0800

    Add code from 1/19 lecture.

--- app/src/main/java/com/example/android/cs492todos/MainActivity.java
+++ app/src/main/java/com/example/android/cs492todos/MainActivity.java
@@ -3,25 +3,44 @@ package com.example.android.cs492todos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText todoEntryET;
     private TextView todoListTV;
+    private ArrayList<String> todoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.todoListTV = findViewById(R.id.tv_todo_list);
-        this.todoListTV.setText("TODO: finish my TODO app");
+        todoList = new ArrayList<>();
 
+        this.todoListTV = findViewById(R.id.tv_todo_list);
         this.todoEntryET = findViewById(R.id.et_todo_entry_box);
 
         Button addTodoBtn = findViewById(R.id.btn_add_todo);
+        addTodoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoText = todoEntryET.getText().toString();
+                if (!TextUtils.isEmpty(todoText)) {
+                    todoEntryET.setText("");
+                    todoList.add(todoText);
+                    todoListTV.setText("");
+                    for (String todo : todoList) {
+                        todoListTV.append(todo + "\n\n");
+                    }
+                }
+            }
+        });
     }
 }
